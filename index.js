22e4259012f74cb709190ad70d71bdfd5fe8059c
Maxime <simon.maxime77@gmail.com>
2019-09-06 17:27:05 +0200

    test

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
-var express = require('express')
+var express = require('express');
 var app = express();
+var bodyParser = require("body-parser"); 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.get('/', function(req, res) {
-  res.send('hello world');
+app.get('/',function(req, res) {
+  res.json({
+    hp : 50,
+    damages : 5,
+    speed : 10,
+    name : 'Bob',
+    methode : req.method});
 });
+
+app.post('/',function(req, res) {
+  res.json({
+  message : "Update Hp",
+  hp : req.body.hp,
+  speed : 10,
+  name : 'Bob',
+  methode : req.method});
+});
+
+
+
 
 app.listen(3000)
